bd8fcea6243117313ca36ddf95f9c47c2dec2f9d
brandononeal <baoneal95@gmail.com>
2021-05-12 09:02:23 -0700

    day 2 complete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,7 +1,5 @@
 """CPU functionality."""
 
-import sys
-
 class CPU:
     """Main CPU class."""
 
@@ -18,33 +16,29 @@ class CPU:
     def ram_write(self, ram_address, ram_value):
         self.ram[ram_address] = ram_value
 
-    def load(self):
+    def load(self, filename):
         """Load a program into memory."""
 
-        address = 0
+        with open(filename) as filedata:
+            address = 0
 
-        # For now, we've just hardcoded a program:
+            for line in filedata:
+                data = line.split('#')[0].strip()
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+                if data == '':
+                    continue
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                num = int(data, 2)
+                self.ram_write(address, num)
+                address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -74,15 +68,16 @@ class CPU:
         self.HLT = 0b00000001
         self.LDI = 0b10000010
         self.PRN = 0b01000111
+        self.MUL = 0b10100010
 
-        operand_a = self.ram_read(self.pc + 1)
-        operand_b = self.ram_read(self.pc + 2)
-        
         running = True
 
         while running:
             inst = self.ram[self.pc]
-            
+
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
             if inst == self.HLT:
                 running = False
             elif inst == self.LDI:
@@ -91,3 +86,6 @@ class CPU:
             elif inst == self.PRN:
                 print(self.reg[operand_a])
                 self.pc += 2
+            elif inst == self.MUL:
+                self.alu('MUL', operand_a, operand_b)
+                self.pc += 3
